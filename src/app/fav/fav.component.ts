d4d0d3d9362d67af9c4c9598d72e3f60884a5001
utparnna dhal <152157927+utparnna@users.noreply.github.com>
2024-07-23 23:07:42 +0530

    Message added for no job added to fav list

--- src/app/fav/fav.component.ts
+++ src/app/fav/fav.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { JobService } from '../job.service';
 import { JobListComponent } from "../job-list/job-list.component";
 import { Job } from '../job.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-fav',
   standalone: true,
-  imports: [JobListComponent],
+  imports: [JobListComponent,CommonModule],
   templateUrl: './fav.component.html',
   styleUrl: './fav.component.css'
 })
